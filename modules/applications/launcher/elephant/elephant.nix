cf1fddd7dd69079a2df86932445e8d9a0f44dc88
Tsvetan Igov <ts.igov@outlook.com>
2025-11-11 18:28:57 +0200

    Updated elephant options

--- modules/applications/launcher/elephant/elephant.nix
+++ modules/applications/launcher/elephant/elephant.nix
@@ -10,5 +10,6 @@ in
 
 		home.file.".config/elephant/desktopapplications.toml".source = ./desktopapplications.toml;
 		home.file.".config/elephant/websearch.toml".source = ./websearch.toml;
+		home.file.".config/elephant/clipboard.toml".source = ./clipboard.toml;
 	};
 }
